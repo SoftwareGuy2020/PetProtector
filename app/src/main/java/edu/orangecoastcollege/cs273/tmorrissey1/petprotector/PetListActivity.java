1f8d2ea5a0f5e3698d6c35541095326bb390f80b
tmorrissey1 <tmorrissey1@student.cccd.edu>
2016-10-25 17:01:30 -0700

    .

--- app/src/main/java/edu/orangecoastcollege/cs273/tmorrissey1/petprotector/PetListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/tmorrissey1/petprotector/PetListActivity.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 
 public class PetListActivity extends AppCompatActivity {
 
+    private static final int RESULT_LOAD_IMAGE = 100;
     private ImageView petImageView;
     // store the uri to whatever image was selected. default is none.png
     private Uri imageURI;
@@ -62,10 +63,9 @@ public class PetListActivity extends AppCompatActivity {
         if (readExternalPermission != PackageManager.PERMISSION_GRANTED)
             permList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
 
-        int requestCode = 100;
         if (permList.size() > 0) {
             String[] perms = new String[permList.size()];
-            ActivityCompat.requestPermissions(this, permList.toArray(perms), requestCode);
+            ActivityCompat.requestPermissions(this, permList.toArray(perms), RESULT_LOAD_IMAGE);
         }
 
         // if we have all 3 permissions, open image gallery
@@ -74,8 +74,18 @@ public class PetListActivity extends AppCompatActivity {
                 && readExternalPermission == PackageManager.PERMISSION_GRANTED) {
 
             Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(galleryIntent, requestCode);
+            startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+
         }
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            imageURI = data.getData();
+            petImageView.setImageURI(imageURI);
+        }
     }
 }
